f032700efe7df286bc99e0de5a4c99459f150afe
arnugroho <nugroho.akbar.r@gmail.com>
2022-06-13 13:42:29 +0700

    add Hello world

---
+++ day1/HelloWorld.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + 1);
+        }
+
+    }
+}
