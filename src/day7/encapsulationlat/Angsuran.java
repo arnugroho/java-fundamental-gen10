49b68aba90008942e3df49488fad48cd70586fae
arnugroho <nugroho.akbar.r@gmail.com>
2022-06-21 11:04:14 +0700

    encapsulation and bean

---
+++ src/day7/encapsulationlat/Angsuran.java
@@ -0,0 +1,13 @@
+package day7.encapsulationlat;
+
+public class Angsuran {
+
+    public void sisaCicilan(){
+        Customer customer = new Customer();
+        Double saldo = customer.getSaldo();
+//        Double saldo2 = customer.saldo;
+        ///
+        //
+        //
+    }
+}
