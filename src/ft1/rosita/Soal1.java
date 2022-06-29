c496006b0984db6d39e46f811ebb581f5f09e79a
arnugroho <nugroho.akbar.r@gmail.com>
2022-06-29 13:32:08 +0700

    ft1 rosita

---
+++ src/ft1/rosita/Soal1.java
@@ -0,0 +1,58 @@
+package ft1.rosita;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Soal1 {
+    
+    public Scanner in = new Scanner(System.in);
+    private int n;
+    private int [][] arr;
+
+    public void getN() {
+        System.out.print("Masukkan Nilai N : ");
+        this.n = in.nextInt();
+    }
+
+    public void isiArray(int n) {
+        this.arr = new int[2][n];
+        int a = 1;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i == 0) {
+                    arr[i][j] = a;
+                    a = a + 2;
+                } else {
+                    a = a - 2;
+                    arr[i][j] = a;
+                }
+            }
+        }
+    }
+
+    public void cetakArray() {
+        Boolean tes = true;
+        do {
+            getN();
+            isiArray(n);
+            for (int i = 0; i < 2; i++) {
+                System.out.println(Arrays.toString(arr[i]));
+            }
+
+            System.out.print("\n\nApakah anda ingin mengulang (Y/N)?");
+            String ulang = in.next().toLowerCase();
+            if (ulang.equalsIgnoreCase("Y")) {
+                tes = true;
+            } else {
+                tes = false;
+            }
+
+        } while (tes);
+        in.close();
+    }
+
+    public static void main(String[] args) {
+        Soal1 cetak = new Soal1();
+        cetak.cetakArray();
+    }
+}
